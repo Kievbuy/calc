bd2a627e2cebfb44c7afb74d4b5b964011454756
Dmytro Kharytonenko <kievbuy@gmail.com>
2019-09-05 17:16:04 +0300

    add methods to controller

--- app.rb
+++ app.rb
@@ -8,44 +8,69 @@ set currencies: [{ code: 'UAH', title: 'гривні' }, { code: 'EUR', title: '
 set time_ranges: [{ code: 'none', title: 'немає' }, { code: 'monthly', title: 'щомісячно' }, { code: 'quarterly', title: 'щоквартально' }, { code: 'annually', title: 'щорічно' }]
 
 get '/' do
-  @data = nil
 	erb :index
 end
 
 post '/' do
-  # binding.pry
   @params = params
-  @data = calculate_deposit(@params)
+  @deposit = params['deposit'].to_f
+  @currency = params['currency']
+  @data, @total_days, @total_income = calculate_deposit(@params)
   erb :index
 end
 
 private
+##########################################################
 
 def calculate_deposit(raw_data)
   months = raw_data["period_type"] == 'years' ? raw_data['period_value'].to_i * 12 : raw_data['period_value'].to_i
   data = {}
   date = Date.parse(raw_data['date'])
-  start_amount = raw_data['deposit'].to_f
 
-  total_days = Integer((date >> months) - date)
-  dayly_rate = (10/100.to_f) / 365
+  current_body = raw_data['deposit'].to_f
+  total_income = current_body
+
+  dayly_rate = (raw_data['annual_rate'].to_f/100) / 365
 
   index = 0
 
-  months.times do
+  (months + 1).times do
     data[index] = {}
     if index.positive?
       days_in_the_month = Integer((date >> 1) - date)
       date = date >> 1
-      data[index][:date] = date
+      data[index][:date] = date.to_s
       data[index][:days_number] = days_in_the_month
-      data[index][:body] = start_amount
-      data[index][:income] = start_amount * (dayly_rate * days_in_the_month)
+
+      income = current_body * (dayly_rate * days_in_the_month)
+      total_income += income
+
+      data[index][:income] = income
+
+      current_body = calculate_capitalisation(data, current_body, index, total_income, raw_data['capitalisation'])
+
+      data[index][:body] = current_body
     else
-      data[index][:date] = date
-      data[index][:body] = start_amount
+      data[index][:date] = date.to_s
+      data[index][:body] = current_body
     end
     index += 1
   end
-  data
+  [data, data.inject(0) { |sum, record| sum += record[1][:days_number].to_i }, total_income]
 end
+
+def calculate_capitalisation(data, body, index, total_income, capitalisation)
+  case capitalisation
+  when 'monthly'
+    body + data[index][:income]
+  when 'quarterly'
+    index % 3 != 0 ? body : total_income
+  when 'annually'
+    index % 12 != 0 ? body : total_income
+  when 'none'
+    body
+  else
+    body
+  end
+end
+
